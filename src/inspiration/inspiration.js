e23258d8d5a1e109dbb8f24091cf83aa3d727bb2
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-06-01 21:43:42 +0200

    second commit

--- src/inspiration/inspiration.js
+++ src/inspiration/inspiration.js
@@ -1,1 +1,28 @@
 import "./inspiration.scss";
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img1 = document.getElementById("myImg1");
+var modalImg1 = document.getElementById("img01");
+var img2 = document.getElementById("myImg2");
+var modalImg2 = document.getElementById("img03");
+var captionText = document.getElementById("caption");
+img1.onclick = function () {
+  modal.style.display = "block";
+  modalImg1.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+img2.onclick = function () {
+  modal.style.display = "block";
+  modalImg2.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
