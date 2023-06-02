2a7122a72e2c8c531be3a5fc980bc0755f45f233
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-06-02 19:55:36 +0200

    modif filter

--- src/produits/produits.js
+++ src/produits/produits.js
@@ -22,3 +22,52 @@ window.onclick = function (event) {
     }
   }
 };
+
+/* filter */
+
+var filterSelectAll = document.querySelector("#filterSelection-all");
+filterSelectAll.addEventListener("click", function () {
+  filterSelection("all");
+});
+
+var filterSelectConfort = document.querySelector("#filterSelection-confort");
+filterSelectConfort.addEventListener("click", function () {
+  filterSelection("confort");
+});
+
+var filterSelectSurface = document.querySelector("#filterSelection-surface");
+filterSelectSurface.addEventListener("click", function () {
+  filterSelection("surface");
+});
+
+var filterSelectDecoration = document.querySelector(
+  "#filterSelection-decoration"
+);
+filterSelectDecoration.addEventListener("click", function () {
+  filterSelection("decoration");
+});
+
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("produit-container-elem");
+  if (c == "all") {
+    c = "";
+  }
+  for (i = 0; i < x.length; i++) {
+    if (c === "" || x[i].querySelector(".filterDiv-" + c)) {
+      x[i].style.display = "block";
+    } else {
+      x[i].style.display = "none";
+    }
+  }
+}
+
+var btnContainer = document.getElementById("myDropdown");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
