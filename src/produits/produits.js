2dd9d0fee057a6ec34dd0997784db5081605659b
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-06-02 18:46:02 +0200

    add produits

--- src/produits/produits.js
+++ src/produits/produits.js
@@ -1,1 +1,24 @@
 import "./produits.scss";
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+var btn = document.querySelector("#myButton");
+btn.addEventListener("click", myFunction);
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
