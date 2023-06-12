df7ce0cd8d4639c05928e6d30618e1fe187a0332
Harry <harry.cuerd01@gmail.com>
2023-06-13 01:20:39 +0200

    services

--- src/serv/services.js
+++ src/serv/services.js
@@ -18,3 +18,16 @@ import "../assets/styles/styles.scss";
 //   evt.currentTarget.classList.add("active");
 //   openTab();
 // }
+
+function openMode(ModeName) {
+
+    var i;
+  
+    var x = document.getElementsByClassName("Mode");
+  
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(ModeName).style.display = "block";
+  
+  }
