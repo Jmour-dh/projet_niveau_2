8e47936232e94e1e1706bde87a21cfc3b51b2765
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-06-13 16:40:41 +0200

    last

--- src/serv/services.js
+++ src/serv/services.js
@@ -1,1 +1,58 @@
 import "../assets/styles/styles.scss";
+import "./services.scss";
+
+var btn_prev = document.querySelector("#btn-prev");
+btn_prev.addEventListener("click", function () {
+  plusSlides(-1);
+});
+var btn_next = document.querySelector("#btn-next");
+btn_next.addEventListener("click", function () {
+  plusSlides(+1);
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
+
+
+
+/* tab */
+
+function openMode(ModeName) {
+
+  var i;
+
+  var x = document.getElementsByClassName("Mode");
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(ModeName).style.display = "block";
+
+}
+
